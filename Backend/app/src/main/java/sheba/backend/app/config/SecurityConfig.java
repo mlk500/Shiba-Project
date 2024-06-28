180a37c3069f151c67d75b05480db0f4b606c6c6
Malak_17 <57171298+mlk500@users.noreply.github.com>
2024-06-28 21:54:03 +0300

    Update SecurityConfig.java

--- Backend/app/src/main/java/sheba/backend/app/config/SecurityConfig.java
+++ Backend/app/src/main/java/sheba/backend/app/config/SecurityConfig.java
@@ -84,11 +84,16 @@ public class SecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173", "http://localhost:8080/",
+//                        "https://admin-platform-9l34-39s7xfz6x-mlk500s-projects.vercel.app/")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8080/",
-                        "https://admin-platform-9l34-39s7xfz6x-mlk500s-projects.vercel.app/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedMethods("*")
+                .allowedOrigins("*");
     }
 }
+
